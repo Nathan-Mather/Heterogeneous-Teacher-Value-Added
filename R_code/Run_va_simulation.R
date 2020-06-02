3af18c31023b7ced3dcfc98dc1e51bebf61e9f39
Nathan-Mather <33639389+Nathan-Mather@users.noreply.github.com>
2020-06-02 11:23:26 -0400

    stat weights

--- R_code/Run_va_simulation.R
+++ R_code/Run_va_simulation.R
@@ -46,7 +46,8 @@ r_dt <- simulate_test_data()
 #=================#
 
 # for now I am just going to use built in commands. If we eventually want to do measurment error correction and stuff 
-# then we will have to write this more from scratch and can rely on the VA code I have from EA. 
+# then we will have to write this more from scratch and can rely on the VA code I have from EA. We will also 
+# have to create a dummy matrix once students have multiple teacher assignments 
 
 # convert teacher_id to a factor so we can treat it as a dummy 
 r_dt[, teacher_id := as.factor(teacher_id)]
@@ -75,4 +76,79 @@ correlation <- va_res[, cor(estimate, teacher_ability)]
 # ==== welfare weights ====
 #==========================#
 
+# run first regression to partial out controlls 
+ww_partial <- lm(test_2 ~ test_1 , data = r_dt)
 
+# get the residuals 
+ww_resids <- data.table(stud_id = r_dt$stud_id, residual = residuals(ww_partial) )
+
+# merge on residuals 
+r_dt <- merge(r_dt, ww_resids, "stud_id")
+
+# make weights 
+# lets do a linear weight where the lowest student counts twice as much as the highest 
+max_score <- r_dt[, max(test_1)]
+min_score <- r_dt[,min(test_1)]
+r_dt[, weight := 2-(test_1-min_score)*(1/(max_score-min_score))]
+summary(r_dt$weight)
+
+# Now run weighted regression 
+ww_va <- lm(residual ~ teacher_id - 1, weights = weight, data = r_dt)
+
+# clean results 
+ww_tab <- data.table(tidy(ww_va))
+
+ww_tab[, teacher_id := gsub("teacher_id", "", term)]
+
+ww_res <- ww_tab[term %like% "teacher_id", c("estimate", "teacher_id")]
+
+# merge it on to data 
+ww_res <- merge(teach_dt, ww_res, "teacher_id")
+
+# get correlation 
+correlation <- ww_res[, cor(estimate, teacher_ability)]
+
+#============================#
+# ==== rescaling weights ====
+#============================#
+# I'm not sure what happens if I rescale the weights so I am going to try that 
+r_dt[, weight_2 := weight*.5]
+summary(r_dt$weight_2)
+
+# Now run weighted regression 
+ww_va2 <- lm(residual ~ teacher_id - 1, weights = weight_2, data = r_dt)
+
+# clean results 
+ww_tab2 <- data.table(tidy(ww_va2))
+
+ww_tab2[, teacher_id := gsub("teacher_id", "", term)]
+
+
+# compare, looks like nothing happens... interesting 
+all.equal(ww_tab2$estimate, ww_tab$estimate)
+all.equal(ww_tab2$std.error, ww_tab$std.error)
+all.equal(r_dt$weight, r_dt$weight_2)
+
+
+#==============================#
+# ==== try extreme weights ====
+#==============================#
+
+r_dt[, weight_3 := (1/(test_1 + abs(min_score) +.01))]
+summary(r_dt$weight_3)
+
+# Now run weighted regression 
+ww_va3 <- lm(residual ~ teacher_id - 1, weights = weight_3, data = r_dt)
+
+# clean results 
+ww_tab3 <- data.table(tidy(ww_va3))
+
+ww_tab3[, teacher_id := gsub("teacher_id", "", term)]
+
+
+# compare:
+all.equal(ww_tab3$estimate, ww_tab$estimate)
+all.equal(ww_tab3$std.error, ww_tab$std.error)
+summary(ww_tab3$std.error)
+summary( ww_tab$std.error)
+=all.equal(r_dt$weight, r_dt$weight_3)
