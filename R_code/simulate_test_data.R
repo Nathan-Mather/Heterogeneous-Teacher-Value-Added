0490aacedd2104c519fb973dd7c2b219821b3b1e
Nathan-Mather <33639389+Nathan-Mather@users.noreply.github.com>
2020-06-19 15:34:49 -0400

    fix teahcer impact

--- R_code/simulate_test_data.R
+++ R_code/simulate_test_data.R
@@ -46,6 +46,9 @@ library(data.table)
   #'@param max_stud maximum students per school
   #'@param n_stud_per_teacher goal number of students per teacher 
   #'@param test_SEM The test's Standard Error of Measure. Maybe eventually want condition SEMS too 
+  #'@param teacher_va_epsilon SE of normal distribution adding noise to how teahcer ability translates into studnet growth 
+  #'@param teacher_abiliy_drop_off This dictates how quickly teacher ability falls as a student moves away from a
+  #'teacher's ideal "center". Higher numbers mean a teachers ability drops off faster. 
   #'@details 
   #'@examples 
 
@@ -66,20 +69,22 @@ library(data.table)
   # #   # parameters for test
   #   test_SEM = .07 # this is a complete guess at this point. Need to brush up on psychometrics
   # 
-  #   # uncertainty for teacher ability translating into student growth 
-  # techer_va_epsilon  = .1
-
+  #   # uncertainty for teacher ability translating into student growth
+  # teacher_va_epsilon  = .1
+  # teacher_abiliy_drop_off     = .25
   #==========================#
   # ==== Define Function ====
   #==========================#
   
   # start of the function
-  simulate_test_data <- function(n_schools          = 20,
-                                 min_stud           = 25,
-                                 max_stud           = 600, 
-                                 n_stud_per_teacher = 25,
-                                 test_SEM           = .07,
-                                 teacher_va_epsilon  = .1){
+  simulate_test_data <- function(n_schools               = 20,
+                                 min_stud                = 25,
+                                 max_stud                = 600, 
+                                 n_stud_per_teacher      = 25,
+                                 test_SEM                = .07,
+                                 teacher_va_epsilon      = .1,
+                                 teacher_abiliy_drop_off = .25){
+    
     # generate an empty list to fill in with schools 
     r_dt <- vector("list", length = n_schools)
     
@@ -119,6 +124,7 @@ library(data.table)
     # assign them a test score 
     #note not 100% sure this is how this works with the SEM but I think this is close to right 
     # mapply applies rnorm to each row using stud_ability_1 and test_SEM from each row
+    # we need to do this because the arguments mean and sd are not vectorized 
     r_dt[, test_1 := mapply(rnorm, n= 1, mean = stud_ability_1, sd = test_SEM) ]
     
     # assign teacher's an overall ability. and a "center" where they perform best 
@@ -129,11 +135,15 @@ library(data.table)
     #note I have no idea what this distribution should look like. keep in mind all the scores are in theory normalized 
     # so a student going down just means relative score is going down 
     n_row_dt <- nrow(r_dt)
-    r_dt[, stud_ability_2 := stud_ability_1 + dnorm(stud_ability_1 - teacher_center)*teacher_ability + rnorm(n_row_dt, sd = teacher_va_epsilon)]
+    # get teacher impact on student without noise 
+    r_dt[, teacher_impact := teacher_ability - abs(stud_ability_1 - teacher_center)* teacher_abiliy_drop_off]
     
-
+    # as impact to student with noise 
+    r_dt[, stud_ability_2 := stud_ability_1 + teacher_impact + rnorm(n_row_dt, sd = teacher_va_epsilon)]
+    
     # Now that they have a new ability give them another test 
     r_dt[, test_2 := mapply(rnorm, n= 1, mean = stud_ability_2, sd = test_SEM) ]
+    
     
     return(r_dt[])
   
@@ -146,6 +156,27 @@ library(data.table)
   # # just leave all the defaults in there 
   # sim_dt <- simulate_test_data()
 
+  #======================#
+  # ==== diognostics ====
+  #======================#
 
+    # take the expected mean teacher ability and center and map their benefit to the draw of students 
+    E_teacher_ability <- 0 
+    E_Teacher_center <- 0 
+    r_dt[, diog_teacher_impact := E_teacher_ability - abs(stud_ability_1 - E_Teacher_center)* teacher_abiliy_drop_off]
+    diog_plot1 <- ggplot(data = r_dt, aes(x= test_1, y = diog_teacher_impact)) + 
+      geom_point() +
+      ggtitle("Expected teacher impact without Noise") +
+      ylab("Theoretical average teacher impact")
+    print(diog_plot1)
+    
+    # now do it with the noise 
+    r_dt[, diog_teacher_impact := E_teacher_ability - abs(stud_ability_1 - E_Teacher_center)* teacher_abiliy_drop_off + rnorm(n_row_dt, sd = teacher_va_epsilon)]
+    diog_plot1 <- ggplot(data = r_dt, aes(x= test_1, y = diog_teacher_impact)) + 
+      geom_point() +
+      ggtitle("Expected teacher impact with Noise") +
+      ylab("Theoretical average teacher impact with added noise ")
+    print(diog_plot1)
+  
 
 
