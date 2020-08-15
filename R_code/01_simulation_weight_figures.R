f5d2c15d96733475c42601487f7581b0a7a1cae7
Nathan-Mather <33639389+Nathan-Mather@users.noreply.github.com>
2020-08-15 17:39:33 -0400

    First plots run with xwalk
    Got the single simulation and weight plots to run with the xwalk

--- R_code/01_simulation_weight_figures.R
+++ R_code/01_simulation_weight_figures.R
@@ -12,6 +12,10 @@ options(scipen = 999)
 cat("\f")
 
 
+# NOTE!!!!!!!! make sure you have these directories created in your out_plot
+# example_teacher_plots
+# Average_teacher_plots
+# weight_plots
 #====================================#
 # ==== set dir and load packages ====
 #====================================#
@@ -20,6 +24,7 @@ cat("\f")
 library(data.table)
 library(ggplot2)
 library(xtable)
+library(gridExtra)
 
 # check users. (NOTE TO MIKE, add something unique to your base working directory to detect when it is your computer)
 my_wd <- getwd()
@@ -47,7 +52,7 @@ if(my_wd %like% "Nmath_000"){
   # set path for stress test data 
   stress_path <- ""
   
-  #set path for plots to save 
+  #set path for plots to save. MAKE SURE YOU HAVE DIRECTORIES CREATED!!!!!!!!!
   out_plot <- ""
   
   
@@ -74,9 +79,9 @@ set.seed(123)
 #=========================#
 
 plot_attributes <- theme_classic() + 
-  theme(text = element_text(size= 65),
-        plot.title = element_text(vjust=0, hjust = 0.5, colour = "black",face = "bold", size =80),
-        plot.subtitle = element_text(color = "black",hjust = 0.5, size =80))
+  theme(text = element_text(size= 20),
+        plot.title = element_text(vjust=0, hjust = 0.5, colour = "black",face = "bold", size =25),
+        plot.subtitle = element_text(color = "black",hjust = 0.5, size =25))
 
 
 
@@ -118,13 +123,37 @@ for(i in 1:nrow(teach_plot_xwalk)){
     E_Teacher_center <- 0
     n_row_dt <- nrow(r_dt)
     
+    # make plot 
     r_dt[, diog_teacher_impact := E_teacher_ability -pmin(abs(stud_ability_1 - E_Teacher_center), 2)* p_teacher_ability_drop_off + rnorm(n_row_dt, sd = p_teacher_va_epsilon)]
-    diog_plot1 <- ggplot(data = r_dt, aes(x= test_1, y = diog_teacher_impact)) +
-      geom_point(size = 6, color = "#ffaabb", alpha = .75) +
-      ylab("Example Teacher's Impact") + 
+    
+    et_impact <- ggplot(data = r_dt, aes(x= test_1, y = diog_teacher_impact)) +
+      geom_point(size = 2, color = "#ffaabb", alpha = .5) +
+      ylab("Teacher's Impact") + 
       xlab("Ex Ante Expected Performance") +
+      ggtitle("Example Teacher's Impact") +
       plot_attributes
 
+    # make table of parameters 
+    parms_tab <- melt.data.table(teach_plot_xwalk[i], measure.vars = colnames(teach_plot_xwalk))
+    
+    # put parameters in with plot
+    parms_tbl <- tableGrob(parms_tab, rows=NULL, cols = NULL, theme = ttheme_default(base_size = 9))
+    et_impact2 <- grid.arrange(et_impact, parms_tbl,
+                            layout_matrix = rbind(c(1, 1, NA),
+                                                  c(1, 1, 2),
+                                                  c(1, 1, NA)))
+    # Make file name 
+    file_name <- paste0(out_plot,
+                        "example_teacher_plots/ET_",
+                        p_n_teacher, "_",
+                        p_n_stud_per_teacher, "_",
+                        p_teacher_va_epsilon, "_",
+                        p_teacher_ability_drop_off, "_",
+                        p_test_SEM)
+    file_name <- gsub("\\.", "", file_name)
+    file_name <- paste0(file_name, ".png")
+    # save it 
+    ggsave(filename = file_name, plot = et_impact2, width = 9, height = 4)
     
     #==============================#
     # ==== mean teacher impact ====
@@ -149,104 +178,158 @@ for(i in 1:nrow(teach_plot_xwalk)){
     r_dt[, ave_teacher_impact := ave_teach_impact_fun(stud_ability_1), stud_id]
     
     # now plot it 
-    diog_plot2 <- ggplot(data = r_dt, aes(x= test_1, y = ave_teacher_impact)) +
-      geom_point(size = 6, color = "#ffaabb", alpha = .75) +
-      ylab("Average Teacher Impact") + 
+    at_impact <- ggplot(data = r_dt, aes(x= test_1, y = ave_teacher_impact)) +
+      geom_point(size = 2, color = "#ffaabb", alpha = .75) +
+      ylab("Average Impact") + 
       xlab("Ex Ante Expected Performance") +
+      ggtitle("Average Teacher Impact") +
       plot_attributes
+    
+    # put it in table with parameters 
+    at_impact2 <- grid.arrange(at_impact, parms_tbl,
+                               layout_matrix = rbind(c(1, 1, NA),
+                                                     c(1, 1, 2),
+                                                     c(1, 1, NA)))
+    # Make file name 
+    file_name <- paste0(out_plot,
+                        "Average_teacher_plots/AT_",
+                        p_n_teacher, "_",
+                        p_n_stud_per_teacher, "_",
+                        p_teacher_va_epsilon, "_",
+                        p_teacher_ability_drop_off, "_",
+                        p_test_SEM)
+    file_name <- gsub("\\.", "", file_name)
+    file_name <- paste0(file_name, ".png")
+    
+    # save it 
+    ggsave(filename = file_name, plot = at_impact2, width = 9, height = 4)
+    
+    
   
 } # close loop over teacher xwalk 
+
 #=======================#
 # ==== weight plots ====
 #=======================#
+
+# simulate data for the weights 
+#NOTE: None of the teacher related inputs are going to matter here 
+# we just want a good discribution of students and student abilities 
+r_dt <- simulate_test_data(n_teacher                = 140,
+                           n_stud_per_teacher       = 100,
+                           test_SEM                 = .07,
+                           teacher_va_epsilon       = .05,
+                           teacher_ability_drop_off = .15)
+
+# get parms for weight plots
+weight_parms <- c("weight_type","lin_alpha", "pctile", "pctile", "v_alpha", "mrpctile", "mrdist")
+
+# start by subsetting to parms for this section and removing duplicates 
+weight_plot_xwalk <- model_xwalk[,weight_parms, with = FALSE]
+weight_plot_xwalk <- unique(weight_plot_xwalk)
+
+# now loop over all unique combos in the xwalk 
+for(i in 1:nrow(weight_plot_xwalk)){
   
-  # get parms for teacher plot 
-  
-  p_pctile = 2 # For linear weights
-  p_pctile = .4 # For rawlsian weights
-  p_v_alpha = 1 # For v weights
-  p_mrpctile = .3 # For mr weights
-  p_mrdist = .2 # for mr weights
-  
-  p_weight_type              <- model_xwalk[i, weight_type]
-  p_lin_alpha                <- model_xwalk[i, lin_alpha] # For linear weights
-  p_pctile                   <- model_xwalk[i, pctile] # for rawlsian 
-  p_v_alpha                  <- model_xwalk[i, v_alpha]# For v weights
-  p_mrpctile                 <- model_xwalk[i, mrpctile] # For mr weights
-  p_mrdist                   <- model_xwalk[i, mrdist] # for mr weights
-  
-  
-  
+  p_weight_type              <- weight_plot_xwalk[i, weight_type]
+  p_lin_alpha                <- weight_plot_xwalk[i, lin_alpha] # For linear weights
+  p_pctile                   <- weight_plot_xwalk[i, pctile] # for rawlsian 
+  p_v_alpha                  <- weight_plot_xwalk[i, v_alpha]# For v weights
+  p_mrpctile                 <- weight_plot_xwalk[i, mrpctile] # For mr weights
+  p_mrdist                   <- weight_plot_xwalk[i, mrdist] # for mr weights
   
   #=============================#
   # ==== linear weight plot ====
   #=============================#
-  
-  r_dt[, linear_weights := linear_weight_fun(alpha = p_pctile, in_test_1 = test_1)]
-  
-  lin_w_plot <- ggplot(data = r_dt, aes(x= test_1, y = linear_weights)) + 
-    geom_point(size = 6, color = "#ffaabb", alpha = .75) + 
-    ylab("Welfare Weight") +
-    xlab("Ex Ante Expected Performance") + xlim(-4,4) +
-    plot_attributes
-  
-  print(lin_w_plot)      
-  
+  if(p_weight_type == "linear"){
+    
+    r_dt[, linear_weights := linear_weight_fun(alpha = p_lin_alpha, in_test_1 = test_1)]
+    
+    weight_plot <- ggplot(data = r_dt, aes(x= test_1, y = linear_weights)) + 
+      geom_point(size = 2, color = "#ffaabb", alpha = .75) + 
+      ylab("Welfare Weight") +
+      xlab("Ex Ante Expected Performance") + xlim(-4,4) +
+      ggtitle(paste0("Linear Weight with ", p_lin_alpha, " slope")) +
+      plot_attributes
+    
+    file_name <- paste0(out_plot,
+                        "weight_plots/linear_",
+                        p_lin_alpha)
+    file_name <- gsub("\\.", "", file_name)
+    file_name <- paste0(file_name, ".png")
+    ggsave(filename = file_name, plot = weight_plot, width = 9, height = 4)
+    
+  }
   
   #===============================#
   # ==== rawlsian weight plot ====
   #===============================#
-  r_dt[, rawlsian_weight := rawlsian_weight_fun(p_pctile, test_1)]
-  
-  rawlsian_w_plot <- ggplot(data = r_dt, aes(x= test_1, y = rawlsian_weight)) + 
-    geom_point(size = 6, color = "#ffaabb", alpha = .75) + 
-    ylab("Welfare Weight") + 
-    scale_y_continuous(breaks = seq(0, 1, by = .25), limits = c(0,1.02)) +
-    xlab("Ex Ante Expected Performance") + xlim(-4,4) +
-    plot_attributes
-  
-  print(rawlsian_w_plot)
+  if(p_weight_type == "rawlsian"){
+    r_dt[, rawlsian_weight := rawlsian_weight_fun(p_pctile, test_1)]
+    
+    weight_plot <- ggplot(data = r_dt, aes(x= test_1, y = rawlsian_weight)) + 
+      geom_point(size = 2, color = "#ffaabb", alpha = .75) + 
+      ylab("Welfare Weight") + 
+      scale_y_continuous(breaks = seq(0, 1, by = .25), limits = c(0,1.02)) +
+      xlab("Ex Ante Expected Performance") + xlim(-4,4) +
+      ggtitle(paste0("Rawlsian Weight at ", p_pctile, " Percentile")) +
+      plot_attributes
+    
+    file_name <- paste0(out_plot,
+                        "weight_plots/rawlsian_",
+                        p_pctile)
+    file_name <- gsub("\\.", "", file_name)
+    file_name <- paste0(file_name, ".png")
+    ggsave(filename = file_name, plot = weight_plot, width = 9, height = 4)
+    
+  }
   #==================================#
   # ==== kernal "mr" weight plot ====
   #==================================#
-  r_dt[,  mr_weight := mr_weight_fun(p_mrpctile, p_mrdist, test_1)]
+  if(p_weight_type == "mr"){
+    
+    r_dt[,  mr_weight := mr_weight_fun(p_mrpctile, p_mrdist, test_1)]
+    
+    weight_plot <- ggplot(data = r_dt, aes(x= test_1, y = mr_weight)) + 
+      geom_point(size = 2, color = "#ffaabb", alpha = .75) + 
+      ylab("Welfare Weight") + ylim(0,1) +
+      xlab("Ex Ante Expected Performance") + xlim(-4,4) +
+      ggtitle(paste0("MR Weight at ", p_mrpctile, " Percenitle and ", p_mrdist, " dist")) +
+      plot_attributes
+    
+    file_name <- paste0(out_plot,
+                        "weight_plots/mr_",
+                        p_mrpctile, "_",
+                        p_mrdist)
+    file_name <- gsub("\\.", "", file_name)
+    file_name <- paste0(file_name, ".png")
+    ggsave(filename = file_name, plot = weight_plot, width = 9, height = 4)
+    
+  }
   
-  mr_w_plot <- ggplot(data = r_dt, aes(x= test_1, y = mr_weight)) + 
-    geom_point(size = 6, color = "#ffaabb", alpha = .75) + 
-    ylab("Welfare Weight") + ylim(0,1) +
-    xlab("Ex Ante Expected Performance") + xlim(-4,4) +
-    plot_attributes
-  
-  print(mr_w_plot)
+  #========================#
+  # ==== V weight plot ====
+  #========================#
 
-#=======================================#
-# ==== save single simulation plots ====
-#=======================================#
-# # save plot 1 
-# png(paste0(paste0(out_plot,"teacher_impact_kernal_noise.png")),
-#     height = 1200, width = 2100, type = "cairo")
-# print(diog_plot1)
-# dev.off()  
-# # save plot 2
-# 
-# png(paste0(paste0(out_plot,"Average_teacher_impact_kernal.png")),
-#     height = 1200, width = 2100, type = "cairo")
-# print(diog_plot2)
-# dev.off()  
-# 
-# # save plot 3 
-# png(paste0(paste0(out_plot,"linear_weight.png")),
-#     height = 1200, width = 2100, type = "cairo")
-# print(lin_w_plot)
-# dev.off() 
-# 
-# # save plot 6
-# png(paste0(paste0(out_plot,"rawlsian_weight.png")),
-#     height = 1200, width = 2100, type = "cairo")
-# print(rawlsian_w_plot)
-# dev.off()  
-# 
-# png(paste0(paste0(out_plot,"Kernal_weight.png")),
-#     height = 1200, width = 2100, type = "cairo")
-# print(mr_w_plot)
-# dev.off() 
+  if(p_weight_type == "v"){
+    
+    r_dt[,  v_weight := v_weight_fun(p_v_alpha, test_1)]
+    
+    weight_plot <- ggplot(data = r_dt, aes(x= test_1, y = v_weight)) + 
+      geom_point(size = 2, color = "#ffaabb", alpha = .75) + 
+      ylab("Welfare Weight")  +
+      xlab("Ex Ante Expected Performance") + xlim(-4,4) +
+      ggtitle(paste0("V Weight with Alpha of ", p_v_alpha)) +
+      plot_attributes
+    
+    file_name <- paste0(out_plot,
+                        "weight_plots/v_",
+                        p_v_alpha)
+    file_name <- gsub("\\.", "", file_name)
+    file_name <- paste0(file_name, ".png")
+    ggsave(filename = file_name, plot = weight_plot, width = 9, height = 4)
+    
+  }
+  
+  
+} # close the loop 
