61269457b9544d31b246358c548f3ccf4b6318b4
Tanner <teastmon@ucsd.edu>
2020-09-21 10:13:19 -0700

    Update to allow covariates

--- R_code/functions/cat_dummies_va.R
+++ R_code/functions/cat_dummies_va.R
@@ -4,7 +4,8 @@ cat_dummies_va <- function(in_data = NULL,
                      in_stud_id    = "stud_id",
                      in_pre_test   = "test_1",
                      in_post_test  = "test_2",
-                     num_cats      = 5){
+                     num_cats      = 5,
+                     reg_formula   = "test_2 ~ test_1 + teacher_id + categories + teacher_id*categories - 1"){
   
   #======================#
   #==== error checks ====#
@@ -24,8 +25,8 @@ cat_dummies_va <- function(in_data = NULL,
   
   # identify the cutpoints based on how many we specified and make the factor
   pctile_cutpoints <- seq(0, 1, length.out=num_cats+1)
-  cutpoints <- quantile(in_data$test_1, pctile_cutpoints)
-  in_data[, categories := as.factor(cut(in_data$test_1, cutpoints))]
+  cutpoints <- quantile(in_data[[in_pre_test]], pctile_cutpoints)
+  in_data[, categories := as.factor(cut(in_data[[in_pre_test]], cutpoints))]
   
   
   #============================#
@@ -34,14 +35,14 @@ cat_dummies_va <- function(in_data = NULL,
   
   
   # run regression 
-  output <- lm(test_2 ~ test_1 + teacher_id + categories + teacher_id*categories - 1, data = in_data)
+  output <- lm(reg_formula, data = in_data)
   
   # clean results 
   output <- data.table(broom::tidy(output))
-  output[, teacher_id := gsub("teacher_id|:categories.*", "", term)]
-  output[, category := gsub("teacher_id[0-9]+|:categories", "", term)]
+  output[, teacher_id := gsub(paste0(in_teacher_id, "|:categories.*"), "", term)]
+  output[, category := gsub(paste0(in_teacher_id, "[0-9]+|:categories"), "", term)]
   
   # Return just the estimates
-  output <- output[term %like% "teacher_id", c("teacher_id", "category", "estimate")]
+  output <- output[term %like% in_teacher_id, c("teacher_id", "category", "estimate")]
   return(output)
 }
