673ec28ed380cb0e8fff88c7a31b8718e1065adb
Tanner <teastmon@ucsd.edu>
2020-10-07 17:04:19 -0700

    Incorporated teacher impact function

--- R_code/functions/teacher_impact.R
+++ R_code/functions/teacher_impact.R
@@ -3,44 +3,61 @@
 #      impact will always be mean 0 and teacher_ability will be the average
 #      impact that a teacher has on their students.
 
-teacher_impact <- function(teacher_ability,
-                           stud_ability_1,
-                           type     = 'MLRN',
-                           func_num = 1)
-  
+teacher_impact <- function(teacher_ability  = NULL,
+                           teacher_center   = NULL,
+                           teacher_max      = NULL,
+                           stud_ability_1   = NULL,
+                           type             = 'MLRN',
+                           func_num         = 1){
+
   # Define magic numbers here so we can build them in later when needed.
-  max_diff = .1 # Maximum difference in impact between best and worst matched student.
-  lin_slope = 1 # For linear functions if we want teachers to have different slopes.
-  exp_num = 6 # How sharply does it jump for students in exponential cases (choose even number).
-  height = 1 # How high does the exponential function go.
-  jump1 = .5 # Where the first kink/discontinuity is in functions.
-  diff_size1 = 1 # Size of the first jump in discontinuous functions.
-  jump2 = .7 # Where the second jump/discontinuity is in functions.
-  diff_size2 = .5 # Size of the second jump in discontinuous functions.
-  amp = 1 # Amplitude in cosine.
-  period = 10 # 2pi/period is the period in cosine.
-  phase = .1 # Is the right phase shift in cosine. 
-  width = .1 # Width of the spike.
+  max_diff <- .1 # Maximum difference in impact between best and worst matched student.
+  lin_slope <- 1 # For linear functions if we want teachers to have different slopes.
+  exp_num <- 6 # How sharply does it jump for students in exponential cases (choose even number).
+  height <- 1 # How high does the exponential function go.
+  jump1 <- .5 # Where the first kink/discontinuity is in functions.
+  diff_size1 <- 1 # Size of the first jump in discontinuous functions.
+  jump2 <- .7 # Where the second jump/discontinuity is in functions.
+  diff_size2 <- .5 # Size of the second jump in discontinuous functions.
+  amp <- 1 # Amplitude in cosine.
+  period <- 10 # 2pi/period is the period in cosine.
+  phase <- .1 # Is the right phase shift in cosine. 
+  width <- .2 # Width of the spike.
   
   
-  # Get the student percentiles.
-  studpct = trunc(rank(stud_ability_1))/len(stud_ability_1)
+  # Get the student ability mean and standard deviation.
+  studmean <- mean(stud_ability_1)
+  studsd <- sd(stud_ability_1)
   
+  
+  # Generate a normalized variable based on sd to plug into the various impacts.
+  studpct <- (abs(stud_ability_1) < studmean + 2*studsd)*
+             ((stud_ability_1 - (studmean - 2*studsd))/(2*(studmean + 2*studsd))) + 
+             (stud_ability_1 >= studmean + 2*studsd)
+  
+  teachpct <- (abs(teacher_center) < 2)*
+    ((teacher_center + 2)/4) + 
+    (teacher_center >= 2)
+  
+  impact <- teacher_ability +
+    (studpct >= teachpct - width)*(studpct < teachpct + width)*
+    (teacher_max - (teacher_max/width)*abs(teachpct - studpct)) -
+    width*teacher_max
   
   # Check which teacher impact function is specified.
   if (type == 'MLR') {
     # MLR - Monotone, Linear, Rank Similar.
     # Upward sloping line
     return(teacher_ability + 
-             lin_slope*max_diff*studpct - 
-             lin_slope*max_diff/2)
+             teacher_max*studpct - 
+             teacher_max/2)
   
   } else if (type == 'ML') {
     # ML - Monotone, Linear, Not Rank Similar.
     # Downward sloping line
-    return(teacher_ability - 
-             lin_slope*max_diff*studpct + 
-             lin_slope*max_diff/2)
+    return(teacher_ability -
+             teacher_max*studpct +
+             teacher_max/2)
     
     
   } else if (type == 'MNoR') {
@@ -48,22 +65,22 @@ teacher_impact <- function(teacher_ability,
     if (func_num == 1) {
       # Parabolic jump
       return(teacher_ability + 
-               height*max_diff*studpct^exp_num - 
-               height*max_diff/(exp_num + 1))
+               teacher_max*studpct^exp_num - 
+               teacher_max/(exp_num + 1))
     
     } else if (func_num == 2) {
       # Parabolic jump then plateau
       return(teacher_ability + 
-               (studpct < jump1)*height*max_diff*((1/jump1)*studpct)^exp_num + 
-               (studpct >= jump1)*height*max_diff - 
-               jump1*height*max_diff/(exp_num + 1) - 
-               (1 - jump1)*height*max_diff)
+               (studpct < teachpct)*teacher_max*((1/teachpct)*studpct)^exp_num + 
+               (studpct >= teachpct)*teacher_max - 
+               teachpct*teacher_max/(exp_num + 1) - 
+               (1 - teachpct)*teacher_max)
       
     } else {
       # Two discontinuous pieces
       return(teacher_ability + 
-               (studpct >= jump1)*(diff_size1*max_diff) - 
-               (1 - jump1)*diff_size1*max_diff)
+               (studpct >= teachpct)*(teacher_max) - 
+               (1 - teachpct)*teacher_max)
     }
     
   
@@ -72,19 +89,19 @@ teacher_impact <- function(teacher_ability,
     if (func_num == 1) {
       # Parabolic drop
       return(teacher_ability + 
-               height*max_diff*(studpct-1)^exp_num - 
-               height*max_diff/(exp_num + 1))
+               teacher_max*(studpct-1)^exp_num - 
+               teacher_max/(exp_num + 1))
       
     } else if (func_num == 2) {
-      # Plateau then parabolic drop
+      # Plateau then parabolic drop ## DOESNT WORK YET
       return(teacher_ability + 
-               (studpct >= jump1)*height*max_diff*((1/jump1)*(studpct-1))^exp_num + 
-               (studpct < jump1)*height*max_diff - 
-               jump1*height*max_diff/(exp_num + 1) - 
-               (1 - jump1)*height*max_diff)
+               (studpct >= teachpct)*teacher_max*((1/teachpct)*(studpct-1))^exp_num + 
+               (studpct < teachpct)*teacher_max - 
+               teachpct*teacher_max/(exp_num + 1) - 
+               (1 - teachpct)*teacher_max)
       
     } else {
-      # Two discontinuous pieces
+      # Two discontinuous pieces ## DOESNT WORK YET
       return(teacher_ability + 
                (studpct < jump1)*(diff_size1*max_diff) - 
                (1 - jump1)*diff_size1*max_diff)
@@ -96,12 +113,12 @@ teacher_impact <- function(teacher_ability,
     if (func_num == 1) {
       # Spike in middle
       return(teacher_ability +
-               (studpct >= jump1 - width)*(studpct < jump1 + width)*
-               (max_diff + (max_diff/width)*abs(jump1 - studpct)) -
-               width*max_diff)
+               (studpct >= teachpct - width)*(studpct < teachpct + width)*
+               (teacher_max - (teacher_max/width)*abs(teachpct - studpct)) -
+               width*teacher_max)
       
     } else if (func_num == 2) {
-      # Three discontinuous pieces
+      # Three discontinuous pieces ## DOESNT WORK YET
       return(teacher_ability + 
                (studpct >= jump1)*(studpct <= jump2)*diff_size1*max_diff +
                (studpct > jump2)*diff_size2*max_diff - 
@@ -109,7 +126,7 @@ teacher_impact <- function(teacher_ability,
                (1 - jump2)*diff_size2*max_diff)
       
     } else if (func_num == 3) {
-      # V-shaped
+      # V-shaped ## DOESNT WORK YET
       return(teacher_ability +
                (studpct < jump1)*(max_diff/jump1)*studpct +
                (studpct >= jump1)*((max_diff/(1-jump1))*studpct - max_diff - max_diff*jump1/(1 - jump1)) +
@@ -118,7 +135,7 @@ teacher_impact <- function(teacher_ability,
                (1 - jump1)*(max_diff + max_diff*jump1/(1 - jump1)))
       
     } else {
-      # Cosine
+      # Cosine ## DOESNT WORK YET
       return(teacher_ability + 
                amp*max_diff*cos(period*(studpct - phase)))
       
@@ -128,4 +145,5 @@ teacher_impact <- function(teacher_ability,
     # MLRN - Monotone, Linear, Rank Similar, No Heterogeneity.
     return(teacher_ability)
   }
+}
   
